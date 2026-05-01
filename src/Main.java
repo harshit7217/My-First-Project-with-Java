90335270934b058c98db61a7b32950658da0dd74
Prasang41 <harshitchaudhary568@gmail.com>
2026-05-01 23:45:19 +0530

    The First version of the program will display a list of all the products with thier prices

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int bubbleGum = 2;
+        float toffee = 0.2f;
+        int iceCream = 5;
+        int chocolate = 4;
+        float doughNut = 2.5f;
+        float panCake = 3.2f;
+
+        System.out.println("Prices:");
+        System.out.println("Bubblegum: $"+ bubbleGum);
+        System.out.println("Toffee: $"+ toffee);
+        System.out.println("Ice cream: $"+ iceCream);
+        System.out.println("Milk chocolate: $"+ chocolate);
+        System.out.println("Doughnut: $"+ doughNut);
+        System.out.println("Pancake: $"+ panCake);
+
+    }
+}
