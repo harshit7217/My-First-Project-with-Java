cb8991ed2abef0a3176fb1a2e19dc0f3bb5d082a
Prasang41 <harshitchaudhary568@gmail.com>
2026-05-02 09:23:13 +0530

    Adding the total earned of each prodcut and income of this period

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        int bubbleGum = 2;
-        float toffee = 0.2f;
-        int iceCream = 5;
-        int chocolate = 4;
-        float doughNut = 2.5f;
-        float panCake = 3.2f;
+//        Print the prices and list of all products
+        int bubbleGumPrice = 2;
+        float toffeePrice = 0.2f;
+        int iceCreamPrice = 5;
+        int chocolatePrice = 4;
+        float doughNutPrice = 2.5f;
+        float panCakePrice = 3.2f;
 
         System.out.println("Prices:");
-        System.out.println("Bubblegum: $"+ bubbleGum);
-        System.out.println("Toffee: $"+ toffee);
-        System.out.println("Ice cream: $"+ iceCream);
-        System.out.println("Milk chocolate: $"+ chocolate);
-        System.out.println("Doughnut: $"+ doughNut);
-        System.out.println("Pancake: $"+ panCake);
+        System.out.println("Bubblegum: $"+ bubbleGumPrice);
+        System.out.println("Toffee: $"+ toffeePrice);
+        System.out.println("Ice cream: $"+ iceCreamPrice);
+        System.out.println("Milk chocolate: $"+ chocolatePrice);
+        System.out.println("Doughnut: $"+ doughNutPrice);
+        System.out.println("Pancake: $"+ panCakePrice);
+
+//        Print the earned the total amount of this period
+        int bubbleGumEarned = 202;
+        int toffeeEarned = 118;
+        int iceCreamEarned = 2250;
+        int milkChocolateEarned = 1680;
+        int doughNutEarned = 1075;
+        int panCakeEarned = 80;
+        float income = bubbleGumEarned + toffeeEarned + iceCreamEarned + milkChocolateEarned + doughNutEarned + panCakeEarned;
+        System.out.println();
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $"+ bubbleGumEarned);
+        System.out.println("Toffee: $"+ toffeeEarned);
+        System.out.println("Ice cream: $"+ iceCreamEarned);
+        System.out.println("Milk chocolate: $"+ milkChocolateEarned);
+        System.out.println("Doughnut: $"+ doughNutEarned);
+        System.out.println("Pancake: $"+ panCakeEarned);
+        System.out.println();
+        System.out.println("Income: $"+ income);
 
     }
 }
